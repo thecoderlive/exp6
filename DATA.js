96fd2d9df52a3db35745b8671725c22b75d9b00c
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 00:04:55 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "489c9e0f-bd4f-40a6-a973-71e93ddf74dd",
+    "home_feed": [
+        {
+            "id": "3b5036df-fff7-4933-9cf2-a24a917c353d",
+            "artist_name": "whether",
+            "home_carousel": [
+                {
+                    "id": "15e6cead-10b2-4eac-9994-726ef50dcbf1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "39641106-930a-4901-aa55-fa6f67b2f620",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "17b5f2cf-bb7f-416f-b120-87df74af2bf2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "f5bab188-f4af-4e85-8d1e-a230be01da8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                },
+                {
+                    "id": "9ef4b4cd-0773-4676-992b-66edda1f1fd0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                }
+            ],
+            "art_caption": "answer",
+            "art_description": "House doctor off federal very guess seem a. Western act step stop. Gun smile stage tree quite."
+        },
+        {
+            "id": "2470cc57-e38f-4dcd-a8ac-8011e50b6e60",
+            "artist_name": "field",
+            "home_carousel": [
+                {
+                    "id": "1ff7b249-ae46-49a0-b159-fa2daef712d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png"
+                },
+                {
+                    "id": "eae12cda-c12f-4459-8810-cc5a1bd052d7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/54.png"
+                },
+                {
+                    "id": "0345144b-c2ee-4e17-bba3-a9ce3f1ecdde",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "72ffb2c8-c413-4b99-b989-8a497241fdff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "3f9df541-5b0e-4311-9f52-d0b9f1163e62",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                }
+            ],
+            "art_caption": "challenge",
+            "art_description": "Language maintain bank fly age course trial while. Various possible prevent just treatment."
+        },
+        {
+            "id": "2aa8e782-c191-4d9b-a9bd-eba01aea731d",
+            "artist_name": "agent",
+            "home_carousel": [
+                {
+                    "id": "b7691f2a-774a-498c-aaed-de2bd7a7bb19",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "fa05b190-9a0f-44a8-8300-777606ff1faf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/189.png"
+                },
+                {
+                    "id": "657b027d-1155-4907-bc91-8dc750df92a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "c2f2116d-0b11-4a46-91d8-89868249152b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "8a0d4914-691d-485b-bc6c-9aa5283eb1f7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                }
+            ],
+            "art_caption": "support",
+            "art_description": "Question next her of sound sort most wait. Economic TV the fill quickly majority force world."
+        },
+        {
+            "id": "99b483b2-3829-4e54-b0b2-ed261fbd2591",
+            "artist_name": "about",
+            "home_carousel": [
+                {
+                    "id": "7dd3e996-ad47-446f-b7b5-87bb47766aba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "3718b991-6127-44e0-87bf-306e0dd61e0a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "05de4cd9-2ef9-4c6a-a978-3262980cf965",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                },
+                {
+                    "id": "f246a613-2b07-42fe-9992-eb971a645a27",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png"
+                },
+                {
+                    "id": "fb909f75-101a-4fc4-993a-0c5c52519a3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                }
+            ],
+            "art_caption": "lay",
+            "art_description": "Everything why child thought detail author region. Television article wall writer military student."
+        },
+        {
+            "id": "5f6756fd-000b-4e74-86a1-e0093d985415",
+            "artist_name": "open",
+            "home_carousel": [
+                {
+                    "id": "09ed0684-7d23-440e-b78a-4b68e2dfcf0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "f5813435-2066-4b0e-af6c-cd99df1f17c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                },
+                {
+                    "id": "3d8e4756-0080-46ac-b0a3-33c4d203f25e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/75.png"
+                },
+                {
+                    "id": "f699ed34-6885-4c3d-be3e-73b2a7915c5b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "3f320a5b-3f77-49b9-a3a9-8baaec6be36c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                }
+            ],
+            "art_caption": "arrive",
+            "art_description": "Direction north edge country dark Congress she major. Audience general along someone behavior."
+        }
+    ]
+}
